62ee15328b827d22e9724fd14b180e2ea63f9d29
Victor Oz <victorozurumba12@gmail.com>
2023-06-29 11:09:00 +0100

    added icons to tickets

--- src/components/homepage-content/Footer.tsx
+++ src/components/homepage-content/Footer.tsx
@@ -1,26 +1,34 @@
+import { Instagram } from "lucide-react";
+import { Twitter } from "lucide-react";
+import { Linkedin } from "lucide-react";
 function Footer() {
   return (
     <>
-      <div className="flex flex-col place-content-center font-bold ">
-        <div className="flex flex-row place-content-around my-16">
+      <div className="flex flex-col place-content-center ">
+        <div className="flex flex-row place-content-around my-16  font-bold ">
           <div className="">
-            <p>Tickets</p>
-            <p>box box box</p>
+            <h1 className="my-3 text-3xl font-extrabold">Tickets</h1>
+            <div className="flex flex-row gap-3 ">
+            <Instagram />
+            <Twitter />
+            <Linkedin />
+            </div>
           </div>
           <div>
-            <p>Privacy Policy</p>
+            <p className="my-3">Privacy Policy</p>
             <p>Terms of Service</p>
           </div>
           <div>
-            <p>Partners</p>
+            <p className="my-3">Partners</p>
             <p>Promotions</p>
           </div>
           <div>
-            <p>Contact</p>
+            <p className="my-3">Contact</p>
             <p>FAQ</p>
           </div>
         </div>
-        <p className="py-4 content-center bg-indigo-600 ">Copyright 2023 Tickets, All Rights Reserved</p>
+        <div className=" w-screen bg-indigo-600 flex justify-center">
+          <p className="py-4  color-white ">Copyright 2023 Tickets, All Rights Reserved</p></div>
       </div>
     </>
   );
--- src/components/homepage-content/JoinUs.tsx
+++ src/components/homepage-content/JoinUs.tsx
@@ -3,9 +3,9 @@ import Button from "@/components/ui-elements/button";
 export default function JoinUs() {
   return (
     <>
-      <div>
+      <div className="flex flex-col items-center  border-2 border-black rounded-2xl mx-10 py-16">
         <div></div>
-        <div>
+        <div >
           <h1 className="text-4xl font-extrabold my-5">Dont miss Updates from Ticketz</h1>
           <input type="text" />
           <Button buttonType="secondary">See More</Button>
