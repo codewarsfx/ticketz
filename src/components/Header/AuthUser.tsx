e25b633445343c584efe6a9e3a615db74138cb63
codewarsfx <chiderainnocent001@gmail.com>
2023-07-19 16:00:02 +0100

    feat:dynamic user data

--- src/components/Header/AuthUser.tsx
+++ src/components/Header/AuthUser.tsx
@@ -3,29 +3,32 @@ import Image from "next/image";
 import { FC } from "react";
 import AuthUserDropDown from "./AuthUserDropDown";
 import useToggleState from "@/Hooks/useToggleState";
+import useAuth from "@/Hooks/useAuthContext";
 
 interface AuthUserProps {}
 
 const AuthUser: FC<AuthUserProps> = ({}) => {
-    const { isOpen, toggleIsOpen } = useToggleState();
-    
+	const { isOpen, toggleIsOpen } = useToggleState();
+	const user = useAuth();
+
 	return (
 		<div
 			className='flex relative items-center  rounded-md gap-1 group hover:bg-gray-50 p-2  cursor-pointer'
 			onClick={toggleIsOpen}
 		>
-			<p className='text-sm text-gray-600 font-semibold'>Hi Chidera,</p>
+			<p className='text-sm text-gray-600 font-semibold'>
+				Hi {user?.user_metadata.full_name.split(' ')[0]},
+			</p>
 			<div className='relative rounded-full w-[30px] h-[30px] border border-gray-400'>
 				<Image
 					fill
-					src='/assets/img/grid-1.jpg'
+					src={user?.user_metadata.avatar_url}
 					alt='hero-img'
 					style={{ objectFit: "cover", borderRadius: "50%" }}
 				/>
 			</div>
 			<ChevronDown className='w-4 h-4 ml-2' />
-            {isOpen && <AuthUserDropDown />}
-            
+			{isOpen && <AuthUserDropDown />}
 		</div>
 	);
 };
